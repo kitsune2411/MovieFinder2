48ff425545324af026fe91930a4f1c472a437a42
AgungAwan <agungkristiawan24@gmail.com>
2023-06-01 15:24:38 +0800

    edit navbar menu

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,16 +1,17 @@
 <script lang="ts" setup>
 const links = [
-    'Dashboard',
-    'Messages',
-    'Profile',
-    'Updates',
+    'Home',
+    'Movie',
+    'Genre',
+    'About',
+    'Help',
 ]
 </script>
 
 <template>
     <v-app-bar flat>
         <v-container class="fill-height d-flex align-center">
-            <v-avatar class="me-10 ms-4" color="grey-darken-1" size="32"></v-avatar>
+            <!-- <v-avatar class="me-10 ms-4" color="grey-darken-1" size="32"></v-avatar> -->
 
             <v-btn v-for="link in links" :key="link" variant="text">
                 {{ link }}
