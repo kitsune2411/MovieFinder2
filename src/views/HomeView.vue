2ed9d91ff23f9bfe4fe0b2b81524efffb21327be
AgungAwan <agungkristiawan24@gmail.com>
2023-06-01 15:25:56 +0800

    display top movies in card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,26 @@
 <script setup lang="ts">
+import { onBeforeMount } from "vue";
+import { useMoviesStore } from "@/stores/movies";
+import { storeToRefs } from "pinia";
+
+const store = useMoviesStore()
+const { moviesList } = storeToRefs(store)
+const { getTopMoviesList } = store
+
+onBeforeMount(() => {
+  getTopMoviesList() // run get top movies
+})
 </script>
 
 <template>
   <main>
-    ini home
+    <v-row justify="space-around">
+      <div v-for="(movie) in moviesList" :key="movie.id" style="margin-top: 1rem; max-width: 300;">
+        <v-card :title="movie.title">
+          <v-img :width="300" aspect-ratio="16/9" cover :src="movie.image" class="text-white" />
+        </v-card>
+        <!-- {{ `${i + 1}. ${movie.title}` }} -->
+      </div>
+    </v-row>
   </main>
 </template>
