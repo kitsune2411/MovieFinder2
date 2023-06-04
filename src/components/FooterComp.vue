1f35b2e1d76d9a9b2b1504cb5fc7ea73525c8e6d
AgungAwan <agungkristiawan24@gmail.com>
2023-06-04 17:19:52 +0800

    update footer style

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,31 +1,24 @@
 <script lang="ts" setup>
 const icons = [
-        'mdi-facebook',
-        'mdi-twitter',
-        'mdi-linkedin',
-        'mdi-instagram',
-      ]
+  'mdi-facebook',
+  'mdi-twitter',
+  'mdi-linkedin',
+  'mdi-instagram',
+]
 </script>
 
 <template>
-    <v-footer class="d-flex flex-column">
-      <div class="bg-teal d-flex w-100 align-center px-4">
-        <strong>Get connected with us on social networks!</strong>
-  
-        <v-spacer></v-spacer>
-  
-        <v-btn
-          v-for="icon in icons"
-          :key="icon"
-          class="mx-4"
-          :icon="icon"
-          variant="plain"
-          size="small"
-        ></v-btn>
-      </div>
-  
-      <div class="px-4 py-2 bg-black text-center w-100">
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-      </div>
-    </v-footer>
-  </template>
+  <v-footer class="d-flex flex-column pa-0 mt-4">
+    <div class="d-flex w-100 align-center px-4" style="background-color: #ff3d00; color: white;">
+      <strong>Get connected with us on social networks!</strong>
+
+      <v-spacer></v-spacer>
+
+      <v-btn v-for="icon in icons" :key="icon" class="mx-4" :icon="icon" variant="plain" size="small"></v-btn>
+    </div>
+
+    <div class="px-4 py-2 text-center w-100" style="background-color: #171c24;">
+      {{ '©️' + ' ' + new Date().getFullYear() }} — <strong>Vuetify</strong>
+    </div>
+  </v-footer>
+</template>
