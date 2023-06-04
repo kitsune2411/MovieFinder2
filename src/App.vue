2fea19d4b5a54e36584a4c9cbed199e397113037
AgungAwan <agungkristiawan24@gmail.com>
2023-06-04 16:14:37 +0800

    add cachingin main component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import HeaderNav from './components/HeaderNav.vue';
 import FooterComp from './components/FooterComp.vue';
 </script>
 
 <template>
   <v-app>
-    <HeaderNav/>
+    <HeaderNav />
     <v-main>
-      <RouterView />
+      <keep-alive>
+        <RouterView />
+      </keep-alive>
     </v-main>
-    <FooterComp  />
+    <FooterComp />
   </v-app>
 </template>
 
 <style scoped>
-
-
-@media (min-width: 1024px) {
-
-}
+@media (min-width: 1024px) {}
 </style>
